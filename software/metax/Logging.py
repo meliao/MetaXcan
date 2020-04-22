b042a9976761c353b9fb5be236b659f57c63d030
meliao <owenjmelia@gmail.com>
2020-04-21 20:59:52 -0500

    Change to logging to see time

--- software/metax/Logging.py
+++ software/metax/Logging.py
@@ -15,7 +15,7 @@ def configureLogging(level=5, target=sys.stderr):
     # create console handler and set level to info
     handler = logging.StreamHandler(target)
     handler.setLevel(level)
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
